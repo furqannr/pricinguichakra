23a0e20b2cad626f96af9744061b979b3424f21f
furqannr <furqannr@gmail.com>
2023-02-28 20:18:54 +0500

    ChatGPTPricingUI

---
+++ app/Header.tsx
@@ -0,0 +1,11 @@
+import { Box, Heading, Text } from '@chakra-ui/react'
+import React from 'react'
+
+export default function Header() {
+  return (
+    <Box textAlign={'center'} bgColor={'#6B46C1'} pt={'80px'} pb={'100px'} color={'White'}>
+        <Heading pb={'10px'}>Simple pricing for your business</Heading>
+<Text>Plans that are carefully crafted to suit your business</Text>
+    </Box>
+  )
+}
---
+++ app/Pricing.tsx
@@ -0,0 +1,52 @@
+import { Box, Button, Heading, HStack, Text, Icon } from "@chakra-ui/react";
+import React from "react";
+import check from "./icons/check";
+
+export default function Pricing() {
+  return (
+    <Box
+      bgColor={"Black"}
+      display={"flex"}
+      mx={'auto'}
+      mt={'-60px'}
+      justifyContent={"center"}
+      alignContent={"center"}
+      height={"270px"}
+      width={'800px'}
+    >
+      {/* Price box*/ }
+      <Box bgColor={"#ad9ec9"} p={'60px'} textAlign={'center'}>
+        <Text fontWeight={"bold"} fontSize={"24px"}>
+          Premium PRO
+        </Text>
+        <Heading fontWeight={"extrabold"} fontSize="60px">
+          $329
+        </Heading>
+        <Text m='10px'>Billed just once</Text>
+        <Button bgColor={"#6B46C1"} color={"white"} w={'300px'}>
+          Get Started
+        </Button>
+      </Box>
+      {/* FeATURE box*/ }
+      <Box bgColor={"White"} pt={"50px"} pl={"25px"} >
+        <Text>Access these features when you buy this pricing package</Text>
+      <HStack m={'5px'} pt={'20px'}>
+      <Icon as ={check}></Icon>
+        <Text>International messaging and calling API</Text>
+        </HStack>
+        <HStack m={'5px'}>
+        <Icon as ={check}></Icon>
+      <Text >Additional phone numbers</Text>
+      </HStack>
+        <HStack m={'5px'}>
+      <Icon as ={check}></Icon>
+      <Text>Additional personal messages option</Text>
+      </HStack>
+        <HStack m={'5px'}>
+      <Icon as ={check}></Icon>
+      <Text>Secured environment keeping secrets</Text>
+      </HStack>
+      </Box>
+    </Box>
+  );
+}
