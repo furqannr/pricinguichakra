7c9cb5344d23ae6869458b0d7c6db07fe17089f5
furqannr <furqannr@gmail.com>
2023-02-28 20:25:45 +0500

    Pricing set

--- app/Pricing.tsx
+++ app/Pricing.tsx
@@ -8,14 +8,14 @@ export default function Pricing() {
       bgColor={"Black"}
       display={"flex"}
       mx={'auto'}
-      mt={'-60px'}
+      mt={'-75px'}
       justifyContent={"center"}
       alignContent={"center"}
       height={"270px"}
       width={'800px'}
     >
       {/* Price box*/ }
-      <Box bgColor={"#ad9ec9"} p={'60px'} textAlign={'center'}>
+      <Box bgColor={"#ad9ec9"} p={'60px'} textAlign={'center'} width={'300px'}>
         <Text fontWeight={"bold"} fontSize={"24px"}>
           Premium PRO
         </Text>
@@ -23,12 +23,12 @@ export default function Pricing() {
           $329
         </Heading>
         <Text m='10px'>Billed just once</Text>
-        <Button bgColor={"#6B46C1"} color={"white"} w={'300px'}>
+        <Button bgColor={"#6B46C1"} color={"white"} w={'200px'}>
           Get Started
         </Button>
       </Box>
       {/* FeATURE box*/ }
-      <Box bgColor={"White"} pt={"50px"} pl={"25px"} >
+      <Box bgColor={"White"} pt={"50px"} pl={"25px"} width={'500px'}>
         <Text>Access these features when you buy this pricing package</Text>
       <HStack m={'5px'} pt={'20px'}>
       <Icon as ={check}></Icon>
